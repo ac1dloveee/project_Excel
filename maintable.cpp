b27eabc7d7e4e25fba68eed2f8c7e7c3dcbc4b72
grimchik <112716265+grimchik@users.noreply.github.com>
2023-02-26 22:33:59 +0300

    f

--- maintable.cpp
+++ maintable.cpp
@@ -12,10 +12,10 @@ MainTable::MainTable(QWidget *parent)
 ui->tableWidget->setColumnCount(1500);
 for (int i =0 ; i <1500;i++){
     std::string str = std::to_string(i+64);
-  // ui->tableWidget->setHorizontalHeaderLabels(QStringList()<<"A"<<"B"<<"C"<<"D"<<"E"<<"F"<<"G"<<"H"<<"I"<<"J"<<"K"<<"L"<<"M"<<"N"<<"O"<<"P"<<"Q"<<"R"<<"S"<<"T"<<"U"<<"V"<<"W"<<"X"<<"Y"<<"Z");
+
    ui->tableWidget->setRowCount(i);
-  QString h= QString::fromStdString(str);
-ui->tableWidget->setHorizontalHeaderLabels(QStringList()<<h);}
+  QString h= QString::fromStdString(str);}
+
 \
 }
 MainTable::~MainTable()
