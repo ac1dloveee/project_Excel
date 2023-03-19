491ebb88b50ad1583d15ba8f610aca87cc6c8bb6
grimchik <112716265+grimchik@users.noreply.github.com>
2023-03-19 22:57:01 +0300

    Table ready
    table

--- maintable.cpp
+++ maintable.cpp
@@ -4,26 +4,29 @@
 #include <iomanip>
 #include <QApplication>
 #include <QString>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QTableWidget>
+#include <QTableWidgetItem>
+#include <QHeaderView>
+
 MainTable::MainTable(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainTable)
 {
-   ui->setupUi(this);
-ui->tableWidget->setColumnCount(1500);
-for (int i =0 ; i <1500;i++){
-    std::string str = std::to_string(i+64);
-
-   ui->tableWidget->setRowCount(i);
-  QString h= QString::fromStdString(str);}
-
-\
+            ui->setupUi(this);
+            ui->bee_cell_table-> setRowCount (1500);
+            ui->bee_cell_table-> setColumnCount (1500);
+            QStringList bee_cell_table_list;
+            bee_cell_table_list << "A"<<"B"<<"C" <<"D"<<"F"<<"G"<<"H"<<"I"<<"J"<<"K"<<"L"<<"M"<<"N"<<"O"<<"P"<<"Q"<<"R"<<"S"<<"T"<<"U"<<"V"<<"W"<<"X"<<"Y"<<"Z";
+            for (int i =1 ; i <= 58 ; i++)
+            {
+                bee_cell_table_list << "A"+ QString::number(i) <<"B" + QString::number(i)<<"C" + QString::number(i) <<"D" + QString::number(i)<<"F" + QString::number(i)<<"G" + QString::number(i)<<"H" + QString::number(i)<<"I" + QString::number(i)<<"J" + QString::number(i)<<"K" + QString::number(i)<<"L" + QString::number(i)<<"M" + QString::number(i)<<"N" + QString::number(i)<<"O" + QString::number(i)<<"P" + QString::number(i)<<"Q"+ QString::number(i)<<"R"+ QString::number(i)<<"S" + QString::number(i)<<"T" + QString::number(i)<<"U" + QString::number(i)<<"V" + QString::number(i)<<"W" + QString::number(i)<<"X" + QString::number(i)<<"Y" + QString::number(i)<<"Z" + QString::number(i);
+            }
+            ui->bee_cell_table->setHorizontalHeaderLabels( bee_cell_table_list);
 }
 MainTable::~MainTable()
 {
     delete ui;
 }
 
-
-
-
-//Fahrat lox i pidor
