3fe5a14dc3698996fae278731c8f245fde965162
grimchik <112716265+grimchik@users.noreply.github.com>
2023-03-30 21:12:46 +0300

    Ctrl + C ver 3.0

--- maintable.cpp
+++ maintable.cpp
@@ -11,6 +11,7 @@
 #include <QHeaderView>
 #include <QClipboard>
 #include <QKeyEvent>
+#include <QTableWidgetItem>
 MainTable::MainTable(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainTable)
@@ -23,6 +24,12 @@ MainTable::MainTable(QWidget *parent)
             for (int i =1 ; i <= 58 ; i++)
             {
                 bee_cell_table_list << "A"+ QString::number(i) <<"B" + QString::number(i)<<"C" + QString::number(i) <<"D" + QString::number(i)<<"F" + QString::number(i)<<"G" + QString::number(i)<<"H" + QString::number(i)<<"I" + QString::number(i)<<"J" + QString::number(i)<<"K" + QString::number(i)<<"L" + QString::number(i)<<"M" + QString::number(i)<<"N" + QString::number(i)<<"O" + QString::number(i)<<"P" + QString::number(i)<<"Q"+ QString::number(i)<<"R"+ QString::number(i)<<"S" + QString::number(i)<<"T" + QString::number(i)<<"U" + QString::number(i)<<"V" + QString::number(i)<<"W" + QString::number(i)<<"X" + QString::number(i)<<"Y" + QString::number(i)<<"Z" + QString::number(i);
+            }
+            for (int i = 0 ; i <=1500;i++){
+                for (int j =0 ; j<=1500 ; j++){
+
+                  ui->bee_cell_table->setItem(i, j, new QTableWidgetItem(""));
+                }
             }
             ui->bee_cell_table->setHorizontalHeaderLabels( bee_cell_table_list);
             //ui->bee_cell_table->setSpan(1,2,3,2); // ОБЪЕДИНЕНИЕ ЯЧЕЕК
@@ -39,32 +46,56 @@ void MainTable::slotShortcutCtrlC()
     QClipboard * clipboard = QApplication::clipboard();
     QList<QTableWidgetItem*> items = ui->bee_cell_table->selectedItems();
     QString Buffer;
-    int Row_1 = 0;
-    int Row_2 = 0;
+    int Row_mn =0 , Row_mx =0;
+    int Column_mn =0 , Column_mx = 0;
     int count = items.count();
+    int Row =0 ;
+    int Column =0;
     for (int i =0 ; i < count ; i++)
     {
-       Row_2 = ui->bee_cell_table->row(items.at(i));
-       QTableWidgetItem *item = items.at(i);
-       if (i == 0)
-       {
-       Row_1 = Row_2;
-       Buffer += item->text();
-       continue;
+       if (i == 0){
+       Row_mn = ui->bee_cell_table->row(items.at(i));
+       Column_mn = ui->bee_cell_table->column(items.at(i));
+       Column_mx = Column_mn;
+       Row_mx = Row_mn;
        }
-       if (Row_1 != Row_2)
-       {
-       Row_1 = Row_2;
-       Buffer +="\n";
+       Row = ui->bee_cell_table->row(items.at(i));
+       Column = ui->bee_cell_table->column(items.at(i));
+
+       if (Column < Column_mn ){
+           Column_mn = Column;
        }
-       else
-       {
-       Buffer +="\t";
+       if (Column > Column_mx){
+           Column_mx =Column;
        }
-       Buffer += item->text();
-       qDebug()<<Buffer;
+       if (Row < Row_mn ){
+           Row_mn = Row;
+       }
+       if (Row > Row_mx){
+           Row_mx =Row;
+       }
     }
-    clipboard->setText(Buffer);
+       qDebug()<<Column_mn<<"MIN";
+       qDebug()<<Column_mx<<"MIN";
+       qDebug()<<Row_mn<<"MIN";
+       qDebug()<<Row_mx<<"MAX";
+       int Row_1 =0 ;
+       int Row_2;
+       int Column_2;
+       for (int i =Row_mn ;i <=Row_mx ; i++){
+           for (int j = Column_mn ; j<=Column_mx;j++){
+               if (ui->bee_cell_table->item(i,j)->text()!= ""){
+               Buffer +=ui->bee_cell_table->item(i,j)->text();}
+
+               else {
+                   Buffer+="\t";
+                    }
+           }
+           if (i != Row_mx){
+           Buffer+="\n";}
+       }
+       qDebug()<<Buffer;
+       clipboard->setText(Buffer);
 }
 
 
